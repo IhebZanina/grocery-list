d96af2da08e2461bc512ea6f5941e9768cfc4df2
Iheb zannina <iheb.zannina@gmail.com>
2024-03-02 18:00:36 +0100

    Wrapping the shopping list

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
-import "./App.css"
+import "./App.css";
+import groceryCart from "./assets/grocery-cart.png";
 const App = () => {
   return (
-    <main className="App"><h1>Hello Iheb</h1></main> 
-  )
-}
+    <main className="App">
+      <div>
+        <div>
+          <h4 className="success">You are done</h4>
+          <div className="header">
+            <h1>Shopping List</h1>
+            <img src={groceryCart} alt="Shopping" />
+            <input
+              type="text"
+              placeholder="Add an Item"
+              className="item-input"
+            />
+          </div>
+        </div>
+        <ul>
+          <li>
+            <div className="container">
+              <input type="checkbox" />
+              <p>Carrots</p>
+            </div>
+            <div>
+              <button className="remove-button">X</button>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </main>
+  );
+};
 
-export default App
+export default App;
